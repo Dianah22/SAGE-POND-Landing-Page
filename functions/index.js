9cdd1917b8475c875ddf451940c3855fe2eba14f
Matovu <matovucaleb2@gmail.com>
2024-04-16 01:05:40 +0300

    bye

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,18 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+admin.initializeApp(); // Initialize Firebase Admin SDK
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+exports.onCreateUser = functions.auth.user().onCreate((user) => {
+  console.log('User created:', user.uid);
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+  // Generate a secure chatId using crypto module (replace with your preferred method)
+  const crypto = require('crypto');
+  const chatId = crypto.randomBytes(16).toString('hex');
+
+  // Update user document in Firestore with the generated chatId
+  const firestore = admin.firestore();
+  return firestore.collection('users').doc(user.uid).update({
+    privateChatId: chatId
+  });
+});
