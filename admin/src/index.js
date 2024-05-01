bded0d59e7fd9fa993c850b256488f99dc8e2c6d
Matovu <matovucaleb2@gmail.com>
2024-05-01 10:10:21 +0300

    ohhhh

--- admin/src/index.js
+++
@@ -1,7 +0,0 @@
-import './style.css';
-import 'SAGE POND/distsage-pond.js';
-import './sidebar';
-import './charts';
-import './dark-mode';
-
-// Have the courage to follow your heart and intuition.
