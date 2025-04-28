397a32e47980ba0c8e99a79c2e6484b71dd123f4
Caleb Matovu <matovucaleb2@gmail.com>
2025-04-29 00:53:30 +0300

    Refactor code structure and remove redundant functions for improved readability and maintainability

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,15 @@ const path = require('path');
 const terserPlugin = require('terser-webpack-plugin')
 module.exports = {
   mode: 'development',
-  entry: './login.js',
+  entry: './js/login.js',
   optimization:{
     minimizer:[new terserPlugin()],
     minimize:true,
   },
   output: {
-    path: path.resolve(__dirname, 'dist/auth'),
-    filename: 'bundle.min.js',
+    path: path.resolve(__dirname, 'js'),
+    filename: 'auth/bundle.login.js',
     clean:true,
   },
-  devtool: 'eval-source-map',
+  devtool: 'source-map', // Use 'source-map' to avoid CSP violations
 };
