6b164b7d78f6da9b1f9eed52046356bd0d083f32
Caleb Matovu <matovucaleb2@gmail.com>
2026-07-13 19:23:50 +0300

    refactor: remove legacy project files and cleanup local repository structure

--- client/js/editor.js
+++
@@ -1,5 +0,0 @@
-var editor1cfg = {}
-	editor1cfg.toolbar = editor1cfg.toolbarMobile = "mytoolbar";
-	editor1cfg.toolbar_mytoolbar = "{html2pdf,insertemoji,insertcode,insertgallery,inserttemplate}";
-
-var editor1 = new RichTextEditor("#editor", { editorResizeMode: "none" },editor1cfg);
